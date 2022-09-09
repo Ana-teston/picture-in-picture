9fc02aaa4c09968c25fa5f50a608c69f491b3a28
Ana-teston <anapaulateston@gmail.com>
2022-09-09 12:04:02 +0200

    add async function for display video elements and addEventListener for button

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('testing');
+ const videoElement = document.getElementById('video');
+ const button = document.getElementById('button');
+
+ //promp to select media stream, pass to video element, then play
+
+ async function selectMediaStream () {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+
+  }
+  catch (error) {
+    console.log('something is wrong here', error); 
+  }
+ }
+
+ button.addEventListener('click', async () => {
+  //disable button
+  button.disable = true;
+  await videoElement.requestPictureInPicture();
+  //resete button
+  button.disable = false;
+ })
+
+   
+
+//on load
+ selectMediaStream();
